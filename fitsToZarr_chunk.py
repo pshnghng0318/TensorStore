97bae8bc0599bc0df4145cd2837ab7118c91be5f
Po-Sheng Huang <128131821+pshnghng0318@users.noreply.github.com>
2025-06-30 09:16:43 +0800

    Update fitsToZarr_chunk.py 0.5GB chunk

--- fitsToZarr_chunk.py
+++ fitsToZarr_chunk.py
@@ -4,17 +4,17 @@ import zarr
 import os
 import shutil
 
-#fits_file = "/images/set_QA/alma_band1_orionkl_bandscan_combined.fits"
-#zarr_dir = "alma16G.zarr"
+fits_file = "../alma_band1_orionkl_bandscan_combined.fits"
+zarr_dir = "../alma16G.zarr"
 #fits_file = "/images/set_QA/HD163296_CO_2_1.fits"
 #zarr_dir = "image.zarr"
-fits_file = "i17_1G.fits"
-zarr_dir = "i17_1G.zarr"
+#fits_file = "i17_1G.fits"
+#zarr_dir = "i17_1G.zarr"
 
 if os.path.exists(zarr_dir):
     shutil.rmtree(zarr_dir)
 
-max_GB = 0.5
+max_GB = 0.25
 max_bytes = max_GB * 1024**3
 
 with fits.open(fits_file, memmap=True) as hdul:
@@ -24,22 +24,23 @@ with fits.open(fits_file, memmap=True) as hdul:
     shape = data.shape
     dtype = data.dtype
     bytes_per_element = data.dtype.itemsize
-    freq_step = int(max(1, max_bytes // (shape[0] * shape[1] * shape[3] * bytes_per_element)))
+    freq_step = int(max(1, max_bytes // (shape[0] * shape[2] * shape[3] * bytes_per_element)))
+    print(shape, dtype, bytes_per_element, freq_step)
 
     zarr_array = zarr.open(
         zarr_dir,
         mode='w',
         shape=shape,
-        chunks=(shape[0], shape[1], freq_step, shape[3]),
+        chunks=(shape[0], freq_step, shape[2], shape[3]),
         dtype=dtype,
         zarr_format=2,
     )
 
-    total_freq = shape[2]
+    total_freq = shape[1]
     for start in range(0, total_freq, freq_step):
         end = min(start + freq_step, total_freq)
         print(start, end)
 
-        chunk = data[:, :, start:end, :]  # 只讀這一段
-        zarr_array[:, :, start:end, :] = chunk
+        chunk = data[:, start:end, :, :]  # 只讀這一段
+        zarr_array[:, start:end, :, :] = chunk
 
