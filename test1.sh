e740d506fc8ca52eba5ef8a83a32740c51d133af
Po-Sheng Huang <128131821+pshnghng0318@users.noreply.github.com>
2025-07-30 15:49:57 +0800

    Update test1.sh

--- test1.sh
+++ test1.sh
@@ -1,16 +1,15 @@
 #!/bin/bash
-#cmake -B build -S .. -DCMAKE_C_COMPILER=/opt/homebrew/bin/gcc-15 -DCMAKE_CXX_COMPILER=/opt/homebrew/bin/g++-15 -DCMAKE_CXX_STANDARD=20 -DCMAKE_PREFIX_PATH="/opt/homebrew/Cellar/tbb/2022.1.0"
+#cmake -B build -S .. -DCMAKE_C_COMPILER=/opt/homebrew/bin/gcc-15 -DCMAKE_CXX_COMPILER=/opt/homebrew/bin/g++-15 -DCMAKE_CXX_STANDARD=20
 #make -j8
-make -j8 read_zarr/fast
+TASK=spectrum
+NCPU=10
+
+make -j8 ${TASK}/fast
 
 sudo purge
 sync && sleep 1
-NCPU=10
-MEM=64
 
-#LOG="result"$NCPU"_chunk"$MEM".log"
-#PNG="result"$NCPU"_chunk"$MEM".png"
-LOG="result"$NCPU"_12G.log"
-PNG="result"$NCPU"_12G.png"
+LOG="${TASK}_result{$NCPU}_12G.log"
+PNG="${TASK}_result{$NCPU}_12G.png"
 
-psrecord "mpirun -np "$NCPU" ./read_zarr" --interval 1 --include-children --log "$LOG" --plot "$PNG"
+psrecord "mpirun -np ${NCPU} ./${TASK}" --interval 0.01 --include-children --log "$LOG" --plot "$PNG"
